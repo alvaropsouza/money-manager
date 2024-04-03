4934e3268245ed2f86c4f3771e23283a9cd9566a
alvaropsouza <alvaropatricio61@gmail.com>
2024-04-03 13:36:34 -0300

    feat: add other components to new transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 import Logo from '../../assets/logo.svg';
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from '../NewTransactionModal';
+
 export function Header() {
   return (
     <header>
       <HeaderContainer>
         <HeaderContent>
           <img src={Logo} alt="" />
-          <NewTransactionButton>Nova Transação</NewTransactionButton>
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <NewTransactionButton>Nova Transação</NewTransactionButton>
+            </Dialog.Trigger>
+
+            <NewTransactionModal />
+          </Dialog.Root>
         </HeaderContent>
       </HeaderContainer>
     </header>
